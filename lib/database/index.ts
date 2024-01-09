8b01bb130bed236b2e97bd24597567863f076e44
MOHAMED <mohamed1239455@gmail.com>
2024-01-09 16:38:33 +0200

    actions

---
+++ lib/database/index.ts
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+let cached = (global as any).mongoose || { conn: null, promise: null };
+
+export const connectToDatabase = async () => {
+  if (cached.conn) return cached.conn;
+
+  if (!MONGODB_URI) throw new Error('MONGODB_URI NOT FOUND');
+
+  cached.promise =
+    cached.process ||
+    mongoose.connect(MONGODB_URI, {
+      dbName: 'e-commerce',
+      bufferCommands: false,
+    });
+
+  cached.conn = await cached.promise;
+
+  return cached.conn;
+};
