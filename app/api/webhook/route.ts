3e56d34debf45cf9f9e420969e4d76d0dc1f46e3
MOHAMED <mohamed1239455@gmail.com>
2024-01-09 16:44:45 +0200

    actions

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -94,13 +94,13 @@ export async function POST(req: Request) {
     return NextResponse.json({ message: 'OK', user: updatedUser });
   }
 
-  if (eventType === 'user.deleted') {
-    const { id } = evt.data;
+  //   if (eventType === 'user.deleted') {
+  //     const { id } = evt.data;
 
-    const deletedUser = await delete User(id!);
+  //     const deletedUser = await deleteUser(id!);
 
-    return NextResponse.json({ message: 'OK', user: deletedUser });
-  }
+  //     return NextResponse.json({ message: 'OK', user: deletedUser });
+  //   }
 
-  return new Response('', { status: 200 });
+  //   return new Response('', { status: 200 });
 }
